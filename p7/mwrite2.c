9e9a62e206d6d2375dcfa57a0054342716259c9d
CJ Maranto <cmaranto07@gmail.com>
2022-12-06 10:08:03 -0500

    p7 fin

--- p7/mwrite2.c
+++ p7/mwrite2.c
@@ -33,25 +33,36 @@ int hostname_lookup(const char *dev, char *hostname){
 
     //buffer for who structs
     struct utmp _utmp;
-
-    //remove /dev/ from string
-    char new_dev[strlen(dev)];
-    for(int i = 5; i < strlen(dev)+1; ++i){        
-        new_dev[i-5] = dev[i]; 
-    }
     
     //read/print who structs into buffer
     while(fread(&_utmp,sizeof(struct utmp),1,fd)){
-        printf("%s\n",_utmp.ut_host);
-        if(strcmp(new_dev,_utmp.ut_line) == 0){
-            strcpy(hostname,_utmp.ut_host);
-            return 0;
+        if(strcmp(dev,_utmp.ut_line) == 0){
+            if(strcmp(_utmp.ut_host,"127.0.0.1") != 0){
+                strcpy(hostname,_utmp.ut_host);
+                return 0;
+            }
         }
     }
 
     //close file
     fclose(fd);
     return 1;
+}
+
+void formatCTime(char *ctimestr, char *formatted){
+    int i;
+    for(i = 11; i < 16; ++i){        
+        formatted[i-11] = ctimestr[i]; 
+    }
+    formatted[i-11] = 0;
+}
+
+void formatTerm(char *termstr, char *formatted){
+    int i;
+    for(i = 5; i < strlen(termstr); ++i){        
+        formatted[i-5] = termstr[i]; 
+    }
+    formatted[i-5]=0;
 }
 
 
@@ -68,21 +79,29 @@ int main(int argc, char *argv[]) {
         char header[HEADERSIZE];
         time_t t = time(NULL);
         char *time_str = ctime(&t);
+        char ftime_str[6];
+        formatCTime(time_str,ftime_str);
+
         char *name = getenv("LOGNAME");
         char *term = ttyname(STDIN_FILENO);
+        char fterm[strlen(term)];
+        formatTerm(term,fterm);
+
+        
         char hostname[UT_HOSTSIZE];
+        if(hostname_lookup(fterm,hostname) != 0){
+            gethostname(hostname,UT_HOSTSIZE);
+        }
         
-        // sprintf(header,"Message from %s@%s on %s at %s ...",
-        //         getenv("LOGNAME"),hostname,ttyname(STDIN_FILENO),time_str);
-        
-        printf("%s\n",name);
-        printf("%s\n",term);
-        hostname_lookup(term,hostname);
-        printf("%s\n",hostname);
-        printf("%s\n",time_str);
+        sprintf(header,"Message from %s@%s on %s at %s ...\n",
+                 name,hostname,fterm,ftime_str);
 
 
         int fd = open(dev,O_WRONLY);
+
+        //write header
+        write(fd,(void*)header,strlen(header));
+
         char buf;
         for(;;){
             read(STDIN_FILENO,(void*)&buf,1);
